97cdee2238bee6546d036a498ba0f05c78b4975a
RedHawk <tadshig@gmail.com>
2020-10-01 19:34:41 +0300

    Two players!

--- gamescript.js
+++ gamescript.js
@@ -22,8 +22,6 @@ function startGame() {
     document.querySelector('.intro').classList.add('fadeOut');
     AiButton.classList.add('fadeOut');
 
-    document.querySelector('.match').classList.add('fadeIn');
-
     if ( AiIsOn ) {
         startMatchPvE();
     }
@@ -31,15 +29,175 @@ function startGame() {
         startMatchPvP();
     }
 }
-/*
+
 // Two players
 function startMatchPvP() {
+    // Score
+    const p1scoreText = document.querySelector('.p1-score p');
+    const p2scoreText = document.querySelector('.p2-score p');
+    // Option buttons
+    const rockButton = document.querySelector('.rock');
+    const paperButton = document.querySelector('.paper');
+    const scissorsButton = document.querySelector('.scissors');
+    // Hands
+    const p1hand = document.querySelector('.p1hand');
+    const p2hand = document.querySelector('.p2hand');
 
+    let p1choice;
+    let p2choice;
+
+    document.querySelector('.turnscreen').classList.add('fadeIn');
+
+    document.querySelector('#readycontinue').addEventListener('click', p1turn);
+
+    function p1turn() {
+        document.querySelector('#readycontinue').removeEventListener('click', p1turn);
+        document.querySelector('.turnscreen').classList.remove('fadeIn');
+        document.querySelector('.match').classList.add('fadeIn');
+        // switching hands
+        p2hand.style.display = 'none';
+        p1hand.style.display = 'block'
+
+        rockButton.addEventListener('click', HandleRock); // Player 1 chosen rock
+        function HandleRock() {
+            p1hand.src = 'gfx/rock.png';
+            p1choice = 'rock';
+            rockButton.removeEventListener('click', HandleRock);
+            paperButton.removeEventListener('click', HandlePaper);
+            scissorsButton.removeEventListener('click', HandleScissors);
+            prepareP2turn();
+        }
+        paperButton.addEventListener('click', HandlePaper); // Player 1 chosen paper
+        function HandlePaper() {
+            p1hand.src = 'gfx/paper.png';
+            p1choice = 'paper';
+            rockButton.removeEventListener('click', HandleRock);
+            paperButton.removeEventListener('click', HandlePaper);
+            scissorsButton.removeEventListener('click', HandleScissors);
+            prepareP2turn();
+        }
+        scissorsButton.addEventListener('click', HandleScissors); // Player 1 chosen scissors
+        function HandleScissors() {
+            p1hand.src = 'gfx/scissors.png';
+            p1choice = 'scissors';
+            rockButton.removeEventListener('click', HandleRock);
+            paperButton.removeEventListener('click', HandlePaper);
+            scissorsButton.removeEventListener('click', HandleScissors);
+            prepareP2turn();
+        }
+    }
+    // This function hides match screen and shows turn screen, prepairing it for player.
+    function prepareP2turn() {
+        document.querySelector('.turnscreen h2').textContent = 'Player 2 turn';
+        document.querySelector('.match').classList.remove('fadeIn');
+        document.querySelector('.turnscreen').classList.add('fadeIn');
+
+        document.querySelector('#readycontinue').addEventListener('click', p2turn);
+    }
+    
+    function p2turn() {
+        // removing button
+        document.querySelector('#readycontinue').removeEventListener('click', p2turn);
+        // switching hands
+        p1hand.style.display = 'none';
+        p2hand.style.display = 'block';
+        // hiding turn screen and showing match screen
+        document.querySelector('.turnscreen').classList.remove('fadeIn');
+        document.querySelector('.match').classList.add('fadeIn');
+
+        rockButton.addEventListener('click', HandleRock); // Player 1 chosen rock
+        function HandleRock() {
+            p2hand.src = 'gfx/rock.png';
+            p2choice = 'rock';
+            rockButton.removeEventListener('click', HandleRock);
+            paperButton.removeEventListener('click', HandlePaper);
+            scissorsButton.removeEventListener('click', HandleScissors);
+            result();
+        }
+        paperButton.addEventListener('click', HandlePaper); // Player 1 chosen paper
+        function HandlePaper() {
+            p2hand.src = 'gfx/paper.png';
+            p2choice = 'paper';
+            rockButton.removeEventListener('click', HandleRock);
+            paperButton.removeEventListener('click', HandlePaper);
+            scissorsButton.removeEventListener('click', HandleScissors);
+            result();
+        }
+        scissorsButton.addEventListener('click', HandleScissors); // Player 1 chosen scissors
+        function HandleScissors() {
+            p2hand.src = 'gfx/scissors.png';
+            p2choice = 'scissors';
+            rockButton.removeEventListener('click', HandleRock);
+            paperButton.removeEventListener('click', HandlePaper);
+            scissorsButton.removeEventListener('click', HandleScissors);
+            result();
+        }
+    }
+
+    function result() {
+        p1hand.style.display = 'block'
+        if ( p2choice == 'rock' && p1choice == 'paper' ) {
+            p1score++;
+            p1scoreText.textContent = p1score;
+            document.querySelector('.winner').textContent = 'Player 1 won round! Press any button to continue';
+        }
+        else if ( p2choice == 'rock' && p1choice == 'scissors' ) {
+            p2score++;
+            p2scoreText.textContent = p2score;
+            document.querySelector('.winner').textContent = 'Player 2 won round! Press any button to continue';
+        }
+        else if ( p2choice == 'paper' && p1choice == 'scissors' ) {
+            p1score++;
+            p1scoreText.textContent = p1score;
+            document.querySelector('.winner').textContent = 'Player 1 won round! Press any button to continue';
+        }
+        else if ( p2choice == 'paper' && p1choice == 'rock' ) {
+            p2score++;
+            p2scoreText.textContent = p2score;
+            document.querySelector('.winner').textContent = 'Player 2 won round! Press any button to continue';
+        }
+        else if ( p2choice == 'scissors' && p1choice == 'rock' ) {
+            p1score++;
+            p1scoreText.textContent = p1score;
+            document.querySelector('.winner').textContent = 'Player 1 won round! Press any button to continue';
+        }
+        else if ( p2choice == 'scissors' && p1choice == 'paper' ) {
+            p2score++;
+            p2scoreText.textContent = p2score;
+            document.querySelector('.winner').textContent = 'Player 2 won round! Press any button to continue';
+        }
+        else {
+            document.querySelector('.winner').textContent = 'Nobody won! Press any button to continue';
+        }
+        if ( p1score >= 10 || p2score >= 10 ) {
+
+            endGame( p1score, p2score );
+        }
+        else {
+            rockButton.addEventListener('click', prepareP1turn);
+            paperButton.addEventListener('click', prepareP1turn);
+            scissorsButton.addEventListener('click', prepareP1turn);
+        }
+    }
+
+    function prepareP1turn() {
+        rockButton.removeEventListener('click', prepareP1turn);
+        paperButton.removeEventListener('click', prepareP1turn);
+        scissorsButton.removeEventListener('click', prepareP1turn);
+        // hiding match screen and preparing it for player
+        document.querySelector('.match').classList.remove('fadeIn');
+        document.querySelector('.winner').textContent = 'Choose option';
+        document.querySelector('.turnscreen h2').textContent = 'Player 1 turn';
+        // showing turnscreen
+        document.querySelector('.turnscreen').classList.add('fadeIn');
+
+        document.querySelector('#readycontinue').addEventListener('click', p1turn);
+    }
 }
-*/
 
 // Player and AI
 function startMatchPvE() {
+    document.querySelector('.match').classList.add('fadeIn');
     // Score
     const p1scoreText = document.querySelector('.p1-score p');
     const p2scoreText = document.querySelector('.p2-score p');
